0df0f05713e6ae56389c6a98db21f9a77d4f445e
jsrn <james.srn@gmail.com>
2014-10-08 22:14:10 +0100

    smart caesar

--- scripts/crypto/caesar.rb
+++ scripts/crypto/caesar.rb
@@ -20,12 +20,37 @@ class Caesar
     text.downcase!
 
     (1..26).each do |num|
-      puts Caesar.new(num).encrypt(text)
+      puts Caesar.new(num).decrypt(text)
     end
   end
 
   # Uses a little magic to determine the most likely plaintext
+  # E is the most common letter in the language, so we can work from there and
+  # do some frequency analysis on our ciphertext to judge the likely key
+  # Alternate idea/code: https://gist.github.com/bt3gl/a8617848ccb37e56034d
   def Caesar.smart_caesar(text)
+    puts "five most likely plaintexts:"
+    text.downcase!
+
+    alphabet = ('a'..'z').to_a.join
+
+    text_mangled = text.gsub(/[^a-z]/,"")
+
+    frequencies = Caesar
+                    .letter_frequencies(text_mangled)
+                    .sort { |a, b| a[1] > b[1] ? -1 : (a[1] < b[1] ? 1 : (a[1] <=> b[1])) }
+
+    (0..5).each do |num|
+      key = (alphabet.index("e") - alphabet.index(frequencies[num][0])).abs
+      puts "#{num}: #{Caesar.new(key).decrypt(text)}"
+    end
+  end
+
+  private
+  def Caesar.letter_frequencies(str)
+    str.downcase.each_char.with_object({}) do |c,h|
+      (h[c] = h.fetch(c,0) + 1) if c =~ /[a-z]/
+    end
   end
 end
 
