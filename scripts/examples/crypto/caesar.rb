0b1391dbb44e9668a1087852ebd686da0e4eacb5
jsrn <james.srn@gmail.com>
2014-10-08 22:29:49 +0100

    moved code to example

---
+++ scripts/examples/crypto/caesar.rb
@@ -0,0 +1,8 @@
+# Example
+ciphertext = "MABL BL TG XQTFIEX HY MAX LEHMADKXP VTXLTK VBIAXK UKXTDXK'L KTP IHPXK!"
+
+puts "[+] dumb caesar example"
+Caesar.dumb_caesar(ciphertext)
+
+puts "\n[+] smart caesar example"
+Caesar.smart_caesar(ciphertext)
--- scripts/lib/crypto/caesar.rb
+++ scripts/lib/crypto/caesar.rb
@@ -53,12 +53,3 @@ class Caesar
     end
   end
 end
-
-# Example
-ciphertext = "MABL BL TG XQTFIEX HY MAX LEHMADKXP VTXLTK VBIAXK UKXTDXK'L KTP IHPXK!"
-
-puts "[+] dumb caesar example"
-Caesar.dumb_caesar(ciphertext)
-
-puts "\n[+] smart caesar example"
-Caesar.smart_caesar(ciphertext)
